c1f634e5ae77396cce2d76832ae6ec4c1cf0893b
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-05-03 14:45:42 +0200

    full

--- script.js
+++ script.js
@@ -104,6 +104,111 @@ console.log(countTotalSalary(salary));
 //#5 Напиши функцію getAllPropValues(arr, prop), яка отримує масив об'єктів і ім'я властивості. Повертає масив значень певної властивості prop з кожного об'єкта в масиві.
 console.log("5 Завдання");
 
+const products = [
+    {
+        name: "Far Cry 1",
+        price: 150,
+        count: 100300,
+        weight: "4gb"
+    },
+    {
+        name: "Far Cry 2",
+        price: 150,
+        count: 201251,
+        weight: "3.5gb"
+    },
+    {
+        name: "Far Cry 3",
+        price: 299,
+        count: 1013225,
+        weight: "15gb"
+    },
+    {
+        name: "Far Cry 4",
+        price: 459,
+        count: 9013225,
+        weight: "30gb"
+    },
+    {
+        name: "Far Cry 5",
+        price: 915,
+        count: 10578295,
+        weight: "40gb"
+    },
+    {
+        name: "Far Cry 6",
+        price: 915,
+        count: 9013225,
+        weight: "30gb"
+    },
+]
+
 const getAllPropValues = function(arr, prop){
-    let result = []
+    let result = [];
+    for (let item of arr) {
+        result.push(item[prop])
+    }
+    return result
 };
+
+console.log(getAllPropValues(products, "name"));
+console.log(getAllPropValues(products, "price"));
+console.log(getAllPropValues(products, "count"));
+console.log(getAllPropValues(products, "weight"));
+
+//#6 Напиши функцію calculateTotalPrice(allProdcuts, productName), яка отримує масив об'єктів та ім'я продукту (значення властивості name). Повертає загальну вартість продукту (ціна * кількість).
+console.log("6 Завдання");
+
+const games = [
+    {
+        name: "Far Cry 1",
+        price: 150,
+        count: 100300,
+        weight: "4gb"
+    },
+    {
+        name: "Far Cry 2",
+        price: 150,
+        count: 201251,
+        weight: "3.5gb"
+    },
+    {
+        name: "Far Cry 3",
+        price: 299,
+        count: 1013225,
+        weight: "15gb"
+    },
+    {
+        name: "Far Cry 4",
+        price: 459,
+        count: 9013225,
+        weight: "30gb"
+    },
+    {
+        name: "Far Cry 5",
+        price: 915,
+        count: 10578295,
+        weight: "40gb"
+    },
+    {
+        name: "Far Cry 6",
+        price: 915,
+        count: 9013225,
+        weight: "30gb"
+    },
+]
+const calculateTotalPrice = function(allProdcuts, productName){
+    for (let i of allProdcuts){
+        if (i.name === productName){
+            const result = i.price * i.count;
+            return `Загальна сума ${i.name} = ${result} грн`
+        }
+    }
+};
+
+console.log(calculateTotalPrice(games, "Far Cry 1"));
+console.log(calculateTotalPrice(games, "Far Cry 2"));
+console.log(calculateTotalPrice(games, "Far Cry 3"));
+console.log(calculateTotalPrice(games, "Far Cry 4"));
+console.log(calculateTotalPrice(games, "Far Cry 5"));
+console.log(calculateTotalPrice(games, "Far Cry 6"));
